451bb918063fb34b42b8b78f36575e88a33cde47
CarlosUCLM <carlos.soto@alu.uclm.es>
2025-11-06 13:23:46 +0100

    creacion e iniciacion de clase Nodo

---
+++ src/Nodo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Node{
+    public final int id;            
+    public final Integer Idpadre; 
+    public final String accion;     
+    public final String estado;      
+    public final int coste;        
+    public final int profundidad;         
+    public final int heuristica;     
+    public final int valor
+}
